337fc14e4064db8d567d9a907909cb2f202c5a78
SebastianNiebl <github@DerSaege.e4ward.com>
2022-08-21 16:40:27 +0200

    Added Properties to TodoItem and removed Namespace

--- TodoItem.cs
+++ TodoItem.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TodoList
+public class TodoItem
 {
-    public class TodoItem
-    {
-    }
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
 }
